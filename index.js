1aebde7647f0f77257101ea18225cbc071736d37
kevin <kthomes13@gmail.com>
2024-01-01 14:08:50 -0600

    fun con

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun()
+saturdayFun("bathe my dog")
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`
+}
+mondayWork()
+mondayWork("work from home.")
+
+function wrapAdjective(flair){
+    return function(para = "special"){
+        return `You are ${flair}${para}${flair}!`
+    }
+} wrapAdjective("*")("a hard worker")
